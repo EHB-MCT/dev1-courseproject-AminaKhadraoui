b8d3e6ce4a26399226d51f0bc850775ecabb8821
Amina Khadraoui <amina.khadraoui@student.ehb.be>
2024-10-03 13:27:23 +0200

    space invader

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,17 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector("canvas");
+   canvas.width = window.innerWidth;
+   canvas.height = window.innerHeight;
+   let context = canvas.getContext("2d");
+
+   function signature() {
+    context.fillRect(0, 0, 300, 300);
+    context.fillStyle = "#AF69EE";
+    context.fillRect(25, 25, 50, 100)
+    context.fillRect(225, 25, 50, 100)
+    context.fillRect(25, 175, 50, 50)
+    context.fillRect(225, 175, 50, 50)
+    context.fillRect(25, 225, 250, 50)
+   }
+   signature()
